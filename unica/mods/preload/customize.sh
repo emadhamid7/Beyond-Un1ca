1144dee5e14782f59ba9db1dbe8a6fa957ce1973
emadhamid7 <emadhamid070@gmail.com>
2026-07-22 18:34:55 +0530

    Undo ksu bs

--- unica/mods/preload/customize.sh
+++ unica/mods/preload/customize.sh
@@ -1,30 +1,21 @@
-# Using KernelSu Next app instead of s-browser bloat
-KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.2.0/KernelSU_Next_v3.2.0_33129-release.apk"
-# https://github.com/tiann/KernelSU/issues/886
-APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
-
-LOG "- Adding KernelSU-Next.apk to preload apps"
-mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
-DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
-
-sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system"
-sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
+# Samsung Internet Browser
+# https://play.google.com/store/apps/details?id=com.sec.android.app.sbrowser
+LOG "- Downloading Samsung Internet app"
+DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.sbrowser")" \
+    "$WORK_DIR/system/system/preload/SBrowser/SBrowser.apk"
 
 while IFS= read -r i; do
-    FILE="$(echo -n "$i" | sed "s.$WORK_DIR/system/..")"
+    i="${i//$WORK_DIR\/system\//}"
 
-    if [ -d "$i" ]; then
-        echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    if [ -d "$WORK_DIR/system/$i" ]; then
+        SET_METADATA "system" "$i" 0 0 755 "u:object_r:system_file:s0"
     else
-        echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+        SET_METADATA "system" "$i" 0 0 644 "u:object_r:system_file:s0"
     fi
 
-    FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
-    echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
-done <<< "$(find "$WORK_DIR/system/system/preload" | sort)"
-
-rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
-while IFS= read -r i; do
-    FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
-    echo "$FILE" >> "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
-done <<< "$(find "$WORK_DIR/system/system/preload" -name "*.apk" | sort)"
+    if [[ "$i" == *".apk" ]] && \
+            ! grep -q "$i" "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"; then
+        LOG "- Adding \"$i\" to /system/system/etc/vpl_apks_count_list.txt"
+        EVAL "echo \"$i\" >> \"$WORK_DIR/system/system/etc/vpl_apks_count_list.txt\""
+    fi
+done <<< "$(find "$WORK_DIR/system/system/preload")"
